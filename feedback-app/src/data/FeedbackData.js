7fb5af3c64f1843cb5819396801b24fdd067381e
skyllex94 <45595147+skyllex94@users.noreply.github.com>
2022-06-08 16:17:59 -0400

    feedback-app init commit

---
+++ feedback-app/src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem aspernatur labore saepe eligendi voluptas."
+    },
+    {
+        id: 2, 
+        rating: 9,
+        text: "orem ipsum, dolor sit amet consectetur adipisicing elit. Eius earum placeat reiciendis, repellat mollitia quae."
+    },
+    {
+        id: 3,
+        rating: 7,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia voluptatem commodi molestias!"
+    }
+]
+
+export default FeedbackData
