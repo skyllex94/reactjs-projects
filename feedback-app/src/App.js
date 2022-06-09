ddf024bfc6936b11bf2a1b49fbf07a72b5ebfcf2
skyllex94 <45595147+skyllex94@users.noreply.github.com>
2022-06-09 12:02:30 -0400

    prop drilling feedbackitem tutorial

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -8,11 +8,17 @@ function App() {
 
     const [feedback, setFeedback] = useState(FeedbackData)
 
+    const deleteFeedBack = (id) => {
+        if (window.confirm("Are you sure you want to delete?")) {
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
+    }
+
     return (
         <>
             <Header />
             <div className='container'>
-                <FeedbackList feedback={feedback} />
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedBack} />
             </div>
         </>
     )
